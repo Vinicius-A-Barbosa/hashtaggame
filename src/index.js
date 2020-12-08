c576511a320f69dbfd71bfcadb1595cc06856090
Vinicius-A-Barbosa <vini.barbosa.a@outlook.com>
2020-12-07 21:27:14 -0300

    inicial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Square extends React.Component{
+  render(){
+    return(
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component{
+  renderSquare(i){
+    return(
+      <Square/>
+    );
+  }
+
+  renderBoard(i,j){
+    var lin = 0;
+    var col = 0;
+    var conta=0;
+    var item = [];
+    var linha = [];
+    for(lin=0;lin<i;lin++){
+      for(col=0;col<j;col++){
+      item.push(<>{this.renderSquare(conta)}</>);
+        conta++;
+      }
+      linha.push(<div className="board-row">{item}</div>);
+      item = []; 
+    }
+    return(
+      <div>
+      {linha}
+      </div>
+    );
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div>
+          {this.renderBoard(3,3)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
